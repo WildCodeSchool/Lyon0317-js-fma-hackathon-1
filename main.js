7843eda7e094dcb88b743d68253d1a94e8e14773
florian leroux <f.j.j.leroux@gmail.com>
2017-03-30 10:47:18 +0200

    add json call to test

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(document).reday(function () {
 
+	let searchURL = 'http://www.omdbapi.com/?s=mask&plot=full';
+
+
+	$.getJSON(searchURL).done(function (movies) {
+		console.log = ('movies');
 
 
 
+	});
 
 
 
